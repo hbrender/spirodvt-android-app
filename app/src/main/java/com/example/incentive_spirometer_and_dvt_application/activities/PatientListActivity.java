71f255dd94645b1f0a69fcbf37925f9313e2e574
Cole deSilva <cole.desilva@gmail.com>
2019-10-26 14:47:02 -0700

    initial login screen work

--- app/src/main/java/com/example/incentive_spirometer_and_dvt_application/activities/PatientListActivity.java
+++ app/src/main/java/com/example/incentive_spirometer_and_dvt_application/activities/PatientListActivity.java
@@ -2,9 +2,13 @@ package com.example.incentive_spirometer_and_dvt_application.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.incentive_spirometer_and_dvt_application.R;
+
+import org.w3c.dom.Text;
 
 public class PatientListActivity extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class PatientListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_list);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String user = intent.getStringExtra("username");
+            TextView test = (TextView) findViewById(R.id.testIntent);
+            test.setText(user);
+        }
+
+
     }
 }
