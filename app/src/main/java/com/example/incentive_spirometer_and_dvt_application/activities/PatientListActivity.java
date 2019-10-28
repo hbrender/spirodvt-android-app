7f4ededa3c32538dbdcd78b546bfa4552737ddfe
Kelsey <kelseylally1@gmail.com>
2019-10-28 03:13:42 -0700

    added patient list activity layout

--- app/src/main/java/com/example/incentive_spirometer_and_dvt_application/activities/PatientListActivity.java
+++ app/src/main/java/com/example/incentive_spirometer_and_dvt_application/activities/PatientListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.incentive_spirometer_and_dvt_application.R;
@@ -12,16 +14,24 @@ import org.w3c.dom.Text;
 
 public class PatientListActivity extends AppCompatActivity {
 
+    // temporary list of fake patients for display testing
+    String[] patientArray = {"Jenny", "Brooke", "James", "Peter", "Mary", "Joseph", "Albert", "Patrick", "Mario", "Harry", "Alice", "Bella", "Fred", "George", "Marshall", "Teegan", "Lauren"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_list);
 
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_patient_listview, patientArray);
+
+        ListView listView = (ListView) findViewById(R.id.patientList);
+        listView.setAdapter(adapter);
+
         Intent intent = getIntent();
         if (intent != null) {
             String user = intent.getStringExtra("username");
-            TextView test = (TextView) findViewById(R.id.testIntent);
-            test.setText(user);
+            //TextView test = (TextView) findViewById(R.id.testIntent);
+            //test.setText(user);
         }
 
 
