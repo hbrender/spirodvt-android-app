0391e42eb31a4f141a13651a7781f3340736e525
Kelsey <kelseylally1@gmail.com>
2019-11-04 23:51:48 -0800

    added new activity for graphing patient data, activity is not yet attached and graphs are not yet functional

---
+++ app/src/main/java/com/example/incentive_spirometer_and_dvt_application/activities/PatientSpirometerInfoActivity.java
@@ -0,0 +1,25 @@
+/*
+Programmed by: Kelsey Lally
+Created on: 11/4
+Last Update: Kelsey Lally, 11/4
+
+ */
+
+
+package com.example.incentive_spirometer_and_dvt_application.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.incentive_spirometer_and_dvt_application.R;
+
+public class PatientSpirometerInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_spirometer_info);
+        //GraphView graph = (GraphView) findViewById()
+    }
+}
